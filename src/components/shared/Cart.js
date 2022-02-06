5edf5f6796f572d3df37b4983353197c89937984
mohammadalimojtahedsoleimani <77240820+mohammadalimojtahedsoleimani@users.noreply.github.com>
2022-02-06 18:35:55 +0330

    shop cart context changed to redux

--- src/components/shared/Cart.js
+++ src/components/shared/Cart.js
@@ -1,7 +1,5 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
-// Context
-import { CartContext } from '../../context/CartContextProvider';
 
 // Functions
 import { shorten } from '../../helper/functions';
@@ -11,29 +9,32 @@ import trashIcon from "../../assets/icons/trash.svg";
 
 // Style
 import styles from "./Cart.module.css";
+import { useDispatch } from "react-redux";
+import { removeItem , decrease , increase } from "../../redux/cart/cartAction";
 
-const Cart = (props) => {
+const Cart = ( { data } ) => {
 
-    const {dispatch} = useContext(CartContext);
-    const {image, title, price, quantity} = props.data;
+    const dispatch = useDispatch ();
+    const { image , title , price , quantity } = data;
 
     return (
-        <div className={styles.container} >
-            <img className={styles.productImage} src={image} />
-            <div className={styles.data}>
-                <h3>{shorten(title)}</h3>
-                <p>{price} $</p>
+        <div className={ styles.container }>
+            <img className={ styles.productImage } src={ image }/>
+            <div className={ styles.data }>
+                <h3>{ shorten ( title ) }</h3>
+                <p>{ price } $</p>
             </div>
             <div>
-                <span className={styles.quantity}>{quantity}</span>
+                <span className={ styles.quantity }>{ quantity }</span>
             </div>
-            <div className={styles.buttonContainer}>
+            <div className={ styles.buttonContainer }>
                 {
-                    quantity > 1 ? 
-                    <button onClick={() => dispatch({type: "DECREASE", payload: props.data})} >-</button> :
-                    <button onClick={() => dispatch({type: "REMOVE_ITEM", payload: props.data})} ><img src={trashIcon} alt="trash" /></button>
+                    quantity > 1 ?
+                        <button onClick={ () => dispatch ( decrease ( data ) ) }>-</button> :
+                        <button onClick={ () => dispatch ( removeItem ( data ) ) }><img
+                            src={ trashIcon } alt="trash"/></button>
                 }
-                <button onClick={() => dispatch({type: "INCREASE", payload: props.data})} >+</button>
+                <button onClick={ () => dispatch ( increase ( data ) ) }>+</button>
             </div>
         </div>
     );
